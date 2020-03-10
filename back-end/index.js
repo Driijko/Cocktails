490bc04a887357cf4ab1be74d5bd9ae6b385d57b
Driijko <driijko@gmail.com>
2020-03-10 12:52:00 -0400

    First commit: basic front end and back end setup

---
+++ back-end/index.js
@@ -0,0 +1,37 @@
+//Require Express
+const express = require('express');
+//Require CORS
+const cors = require('cors');
+// Express Initialize
+const app = express();
+//Express Uses CORS
+app.use(cors());
+
+
+const port = 8000;
+app.listen(port,()=> {
+  console.log('listen port 8000');
+})
+
+//create api
+// app.get('/hello_world', (req,res)=>{
+//   res.send('Hello World');
+// })
+
+app.get('/cocktailList', (req, res)=>{
+  res.send({
+    name: 'Cosmopolitan',
+    ingredients: [
+      'juice',
+      'alchohol',
+      'olives?'
+    ]});
+})
+
+// app.get('/hello/:id', function (req, res, next) {
+//   res.json({msg: 'Hello world, we are CORS-enabled!'});
+// });
+
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server is listening on port 80');
+// });
