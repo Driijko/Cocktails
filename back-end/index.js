e01c495371b49162b063e92da637c353a6596f15
Driijko <driijko@gmail.com>
2020-03-20 19:04:19 -0400

    "Make API calls to cocktail database, receive and parse data, send to front-end"

--- back-end/index.js
+++ back-end/index.js
@@ -1,68 +1,65 @@
-//Require Express
+// Import libraries
 const express = require('express');
-//Require CORS
 const cors = require('cors');
+const axios = require('axios');
+
 // Express Initialize
 const app = express();
-//Express Uses CORS
+// Express Uses CORS
 app.use(cors());
 
-
+// Set up our server
 const port = 8000;
 app.listen(port,()=> {
   console.log('listen port 8000');
 })
 
-//create api
-// app.get('/hello_world', (req,res)=>{
-//   res.send('Hello World');
-// })
-
+// Here we set up the data structure that we will eventually send to the front end.
 const data = [
   {
-    name: 'Cosmopolitan',
-    ingredients: [
-      'juice',
-      'alchohol',
-      'olives?'
-    ]
-  },
-  {
-    name: 'Dark and Stormy',
-    ingredients: [
-      'lime',
-      'ice',
-      'ginger beer',
-      'dark rum'
-    ]
+    name: "margarita",
+    ingredients: []
   }, 
   {
-    name: "Manhattan",
-    ingredients: [
-      'something',
-      'something else'
-    ]
+    name: "cosmopolitan",
+    ingredients: []
   }
-]
+];
 
 app.get('/cocktailList', (req, res)=>{
-  res.send(data);
+  // Step 0: Iterate through each type of cocktail
+  for (let i = 0 ; i < data.length ; i++) {
+
+    // Step 1: Make a bunch of API calls, one for each cocktail
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${data[i].name}`)
+
+    // Step 2: Receive data
+    .then(response => {
+      // console.log(response.data.drinks[0].strIngredient1);
+      // for(let j = 1 ; j < 16 ; j++ ) {
+
+      //   let currentIngredient = `strIngredient${j}`;
+
+      //   data[i].ingredients[j - 1] = response.data.drinks[0][currentIngredient];
+      // }
+
+      // Step 3: Parse data
+      data[i].ingredients[0] = response.data.drinks[0].strIngredient1;
+      if (!(response.data.drinks[0].strIngredient2 === null)) {
+        data[i].ingredients[1] = response.data.drinks[0].strIngredient2;
+      }
+
+
+    })
+    .then(() => {
+      if (i === data.length - 1) {
+        console.log(data);
+        res.send(data);
+      }
+    }
+    )
+    .catch(error => {
+      console.log(error);
+    });
+  }
 })
-
-// app.get('/cocktailList', (req, res)=>{
-//   res.send({
-//     name: 'Cosmopolitan',
-//     ingredients: [
-//       'juice',
-//       'alchohol',
-//       'olives?'
-//     ]});
-// })
-
-// app.get('/hello/:id', function (req, res, next) {
-//   res.json({msg: 'Hello world, we are CORS-enabled!'});
-// });
-
-// app.listen(80, function () {
-//   console.log('CORS-enabled web server is listening on port 80');
-// });
